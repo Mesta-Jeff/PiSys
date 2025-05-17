d34f5383c411490d45c9824626e41e134d402970
Mesta-Jeff <odenehonas1@gmail..com>
2025-05-17 02:14:12 -0700

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Allow access from any IP address
-    // host: '192.168.1.98',  
-    port: 5173 
-  } ,
-  base: '/',
+  // server: {
+  //   host: '0.0.0.0',  // Allow access from any IP address
+  //   // host: '192.168.1.98',  
+  //   port: 5173 
+  // } ,
+  base: '/PiSys/',
 })
